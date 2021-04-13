e83be4ec6025d24f19ccbfeaedd3c853f31663b1
Emily Xie <emilyxie@live.com>
2021-04-12 20:21:53 -0700

    updated for js lessons

---
+++ clean/js/index.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+
+let name = "Emily";
+let greeting = "Hi, my name is" + name;
+console.log(greeting);
+
+
---
+++ prev/js/index.js
@@ -0,0 +1,28 @@
+"use strict";
+// this is a comment
+// use strict is kind of like doctype, it allows you to use the modern version
+// of js
+
+console.log("Hello, World!"); // remember semicolon and quotes
+
+// variables
+let x = 1;
+// x = 1;   no! must use let
+
+console.log(x);  // will now print 1
+
+let y = 4;
+
+let result = x + y;
+console.log(result); // notice that this is not in quotes
+
+// mini exercise answers
+let ans1 = 2740 + 832;
+let ans2 = ans1 - 36;
+
+console.log(ans1);
+console.log(ans2);
+
+let name = "Emily";
+let greeting = "Hi, my name is " + name;
+console.log(greeting)
